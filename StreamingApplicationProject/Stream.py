ace364f5058a5a6e1206cefad8fdcc237f8ee508
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-11-13 16:45:01 +0000

    netwrok streaming over http

--- StreamingApplicationProject/Stream.py
+++ StreamingApplicationProject/Stream.py
@@ -9,37 +9,38 @@ app = Flask(__name__)
 webcamNumber = 0
 sourceInput = False
 source = "Screen"
+loop = True
+# class EchoServerProtocol:
+#     def connection_made(self, transport):
+#         self.transport = transport
+#
+#     def datagram_received(self, data, addr):
+#         message = data.decode()
+#         print('Received %r from %s' % (message, addr))
+#         print('Send %r to %s' % (message, addr))
+#         self.transport.sendto(data, addr)
 
-class EchoServerProtocol:
-    def connection_made(self, transport):
-        self.transport = transport
-
-    def datagram_received(self, data, addr):
-        message = data.decode()
-        print('Received %r from %s' % (message, addr))
-        print('Send %r to %s' % (message, addr))
-        self.transport.sendto(data, addr)
-
-async def server():
-    print("Starting UDP server")
-    # Get a reference to the event loop as we plan to use low-level APIs.
-    loop = asyncio.get_running_loop()
-
-    # One protocol instance will be created to serve all client requests.
-    transport, protocol = await loop.create_datagram_endpoint(
-        lambda: EchoServerProtocol(),
-        local_addr=('127.0.0.1', 9999))
-
-    try:
-        await asyncio.sleep(3600)  # Serve for 1 hour.
-    finally:
-        transport.close()
+# async def server():
+#     print("Starting UDP server")
+#     # Get a reference to the event loop as we plan to use low-level APIs.
+#     loop = asyncio.get_running_loop()
+#
+#     # One protocol instance will be created to serve all client requests.
+#     transport, protocol = await loop.create_datagram_endpoint(
+#         lambda: EchoServerProtocol(),
+#         local_addr=('127.0.0.1', 9999))
+#
+#     try:
+#         await asyncio.sleep(3600)  # Serve for 1 hour.
+#     finally:
+#         transport.close()
 
 def gen_frames():
+    global loop
     #asyncio.run(server())
     if sourceInput == "Webcam":
         camera = cv2.VideoCapture(webcamNumber)
-        while True:
+        while loop:
             success, frame = camera.read()
             if not success:
                 break
@@ -50,49 +51,57 @@ def gen_frames():
                 yield (b'--frame\r\n'
                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
     else:
-        while True:
+        while loop:
             screenGrab = np.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))  # x, y, w, h
             frame = cv2.cvtColor(screenGrab, cv2.COLOR_BGR2RGB)  # Convert PIL screen grab to cv2 colours
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    if not loop:
+        loop = True
+        gen_frames()
 
-class EchoClientProtocol(asyncio.Protocol):
-    def __init__(self, message, on_con_lost):
-        self.message = message
-        self.on_con_lost = on_con_lost
-
-    def connection_made(self, transport):
-        transport.write(self.message.encode())
-        print('Data sent: {!r}'.format(self.message))
-
-    def data_received(self, data):
-        print('Data received: {!r}'.format(data.decode()))
-
-    def connection_lost(self, exc):
-        print('The server closed the connection')
-        self.on_con_lost.set_result(True)
-
-async def client():
-    # Get a reference to the event loop as we plan to use low-level APIs.
-    loop = asyncio.get_running_loop()
-    on_con_lost = loop.create_future()
-    message = 'Hello World!'
-
-    transport, protocol = await loop.create_connection(
-        lambda: EchoClientProtocol(message, on_con_lost),
-        '127.0.0.1', 8888)
-    # Wait until the protocol signals that the connection is lost and close the transport.
-    try:
-        await on_con_lost
-    finally:
-        transport.close()
+# class EchoClientProtocol(asyncio.Protocol):
+#     def __init__(self, message, on_con_lost):
+#         self.message = message
+#         self.on_con_lost = on_con_lost
+#
+#     def connection_made(self, transport):
+#         transport.write(self.message.encode())
+#         print('Data sent: {!r}'.format(self.message))
+#
+#     def data_received(self, data):
+#         print('Data received: {!r}'.format(data.decode()))
+#
+#     def connection_lost(self, exc):
+#         print('The server closed the connection')
+#         self.on_con_lost.set_result(True)
+#
+# async def client():
+#     # Get a reference to the event loop as we plan to use low-level APIs.
+#     loop = asyncio.get_running_loop()
+#     on_con_lost = loop.create_future()
+#     message = 'Hello World!'
+#
+#     transport, protocol = await loop.create_connection(
+#         lambda: EchoClientProtocol(message, on_con_lost),
+#         '127.0.0.1', 8888)
+#     # Wait until the protocol signals that the connection is lost and close the transport.
+#     try:
+#         await on_con_lost
+#     finally:
+#         transport.close()
 
 def get_frames():
     # Loop and receive bytes to make frames with from the server
-    asyncio.run(client())
+    #asyncio.run(client())
     print("Waiting. . . ")
+    # frame = adwnikoawdonijadwnoja;
+    # ret, buffer = cv2.imencode('.jpg', frame)
+    # frame = buffer.tobytes()
+    # yield (b'--frame\r\n'
+    #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
@@ -101,6 +110,16 @@ def video_feed():
         return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
         return Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/changeSource')
+def changeSource():
+    global source, loop
+    if source == 'Screen':
+        source = 'Webcam'
+    else:
+        source = 'Screen'
+    loop = False
+    return ('nothing')
 
 @app.route('/')
 def index():
--- StreamingApplicationProject/main.py
+++ StreamingApplicationProject/main.py
@@ -11,20 +11,25 @@ def inputOption(text, successList):
 
 
 def inputs():
-    if inputOption("Are you joining or hosting a stream (j/h)? ", ['h', 'H']):
-        streamScript.source = True
-        if inputOption("Would you like to stream your webcam or screen (w/s)? ", ['w', 'W']):
-            choice = input("Which webcam would you like to use (0 for primary, 1 for secondary etc etc)? ")
-            streamScript.webcamNumber = int(choice)
-            streamScript.sourceInput = "Webcam"
-            print("Showing Webcam " + str(streamScript.webcamNumber))
-        else:
-            streamScript.sourceInput = "Screen"
-            print("Showing screen")
-    else:
-        streamScript.source = False
+    # if inputOption("Are you joining or hosting a stream (j/h)? ", ['h', 'H']):
+    #     streamScript.source = True
+    #     if inputOption("Would you like to stream your webcam or screen (w/s)? ", ['w', 'W']):
+    #         choice = input("Which webcam would you like to use (0 for primary, 1 for secondary etc etc)? ")
+    #         streamScript.webcamNumber = int(choice)
+    #         streamScript.sourceInput = "Webcam"
+    #         print("Showing Webcam " + str(streamScript.webcamNumber))
+    #     else:
+    #         streamScript.sourceInput = "Screen"
+    #         print("Showing screen")
+    # else:
+    #     streamScript.source = False
 
+    #############################################
+    #Remove after testing
+    streamScript.source = True
+    streamScript.sourceInput = "Screen"
+    #############################################
 
 if __name__ == '__main__':
     inputs()
-    streamScript.app.run(debug=False)
+    streamScript.app.run(host='0.0.0.0', port=5000, debug=False)
