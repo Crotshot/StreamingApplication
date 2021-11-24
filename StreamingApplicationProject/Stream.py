9be84f9fee5440937c41e3c5ea223c88983be1c5
Crotshot <46968070+Crotshot@users.noreply.github.com>
2021-11-24 16:20:18 +0000

    .

--- StreamingApplicationProject/Stream.py
+++ StreamingApplicationProject/Stream.py
@@ -1,111 +1,115 @@
-import ctypes.wintypes
+from PIL import ImageGrab, Image  # Used for screenshotting for screen streaming
+import numpy as np  # Used after grabbing screen for image processing
+from flask import Flask, render_template, Response #Python mini web framework
+#from numba import jit, cuda #Used to put code on the GPU for faster processing (Disable as could not get functioning)
+from nudenet import NudeDetector # Used to censor the images
+import cv2  # Image processing
+import os  # Changing working directory
 
-from PIL import ImageGrab, Image #Used for getting the the screen
-import numpy as np #Used after grabbing screen
-from flask import Flask, render_template, Response
-import cv2
-import os
-
-from nudenet import NudeDetector
-detector = NudeDetector()
-
-from numba import jit, cuda
-app = Flask(__name__)
-
-webcamNumber = 0
-sourceInput = "Webcam"
-webcamChanged = True
-playing = True
-background = None
-motionTracking = False
-refresh = 30
-censoring = False
-cwd = os.path.dirname(os.path.abspath(__file__))
-os.chdir(cwd)
+app = Flask(__name__) # Flask application
 
 
+webcamNumber = 0 #Index of webcam
+refresh = 30 #Variable used to refresh motion capture background
+
+sourceInput = "Screen" # Source input of stream, Webcam or Screen
+
+webcamChanged = True # Used to update the current camera
+playing = True #Toggle for pause/playing the stream
+motionTracking = False # Toggle motion tracking
+censoring = False # Toggle censoring
+backgroundRefresh = True # Toggle motion background refreshing
+
+cwd = os.path.dirname(os.path.abspath(__file__)) # Get path of current file
+os.chdir(cwd) # Change working Directory to directory of this file
+
+
+# <editor-fold desc="Frame generation">
 def gen_frames():
-    global sourceInput, refresh, motionTracking, webcamChanged, background, censoring
-    censorParts = detector.detect("in/frame.jpg", "fast")
-    webcamChanged = True
-    cam = cv2.VideoCapture(webcamNumber)
+    global sourceInput, refresh, motionTracking, webcamChanged, censoring, backgroundRefresh, webcamNumber
+    background = None # Background
+
+    cam = cv2.VideoCapture(webcamNumber) # Assign camera
+
+    det = NudeDetector()  # NudeDetector constructor
+    censorParts = det.detect("in/frame.jpg", "fast") # Generate first censored frame
+
     while True:
         if not playing:
             continue
-        if webcamChanged:
+        if webcamChanged: # Update webcam
             cam = cv2.VideoCapture(webcamNumber)
             webcamChanged = False
 
         refC = False
-        if refresh == 30:
+        if refresh == 30: #Refresh Censor everytime refresh is 30 (Every 60 frames)
             refC = True
+        #preByte is frame before it is turned to bytes and frame is after it is turned to bytes
+        preByte, frame = frameCalc(sourceInput, cam, refresh, censoring, censorParts, refC, background, motionTracking, det)
 
-        preByte, frame = frameCalc(sourceInput, cam, refresh, censoring, censorParts, refC, background, motionTracking)
-
-        if type(preByte) == bool:
+        if type(preByte) == bool: #If camera was not successfully read it frameCalc returns a boolean
             continue
         else:
-            if refresh == 0:
-                background = refBackground(preByte)
+            if refresh == 0:#Refresh motion capture background every 60 frames
+                if backgroundRefresh and sourceInput == "Webcam":
+                    background = refBackground(preByte) #Use frame before it is turned to bytes for background refreshing
                 refresh = 60
             else:
-                refresh = refresh - 1
+                refresh -= 1
 
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Add byte frame, then show it
 
-    #os.remove("in/frame.jpg")
+    os.remove("in/frame.jpg") # Remove left over in and out frames when stopping the stream
     os.remove("out/frame.jpg")
 
-@jit
-def refBackground(frame):
+# @jit#(target = "cuda")
+def refBackground(frame): # Motion censor background is a gray scaled image with a blur
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     backg = cv2.GaussianBlur(gray, (21, 21), 0)
     return backg
 
-@jit
-def frameCalc(source, camera, ref, censor, cParts, refCen, backg, motC):
+# @jit#(target = "cuda")
+def frameCalc(source, camera, ref, censor, cParts, refCen, backg, motC, detector):
     if source == "Webcam":
         success, frame = camera.read()
-        if success:
+        if success: # If Webcam try to read active camera and flip result , if fails returns false
             frame = cv2.flip(frame, 1)
         else:
             return False, False
-    else:
-        screenGrab = np.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))  # x, y, w, h
-        # screenGrab = np.array(ImageGrab.grab(bbox=(-1920, 0, 1920, 1080)))  # x, y, w, h
+    elif source == "Screen": # Grab screen and change colour to rgb from bgr (A little slow as it is done currently on the CPU)
+        screenGrab = np.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))  # x, y, width, height
         frame = cv2.cvtColor(screenGrab, cv2.COLOR_BGR2RGB)  # Convert PIL screen grab to cv2 colours
-    # if stop:
-    #     return
 
-    if motC:
-        countK = 0
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        subtraction = cv2.absdiff(backg, gray)
+
+    if motC and source == "Webcam": # Motion capture for webcam
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # Grayscale frame
+        gray = cv2.GaussianBlur(gray, (21, 21), 0) # Blur gray frame
+        subtraction = cv2.absdiff(backg, gray) # Subtract background and gray frame
         threshold = cv2.threshold(subtraction, 55, 255, cv2.THRESH_BINARY)[1]
-        contourimg = threshold.copy()
+        contourimg = threshold.copy() # Compare differences and if an area is large enough get an outline for it
         outlines, hierarchy = cv2.findContours(contourimg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for c in outlines:
             if cv2.contourArea(c) < 6000:
                 continue
             (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) # Draw all the outlines over the frame
 
     if censor:
-        print(ref)
-        cv2.imwrite("in/frame.jpg", frame)
+        print(ref) # Censoring is currently done on CPU and is very slow, printing refresh here for debugging purposes
+        cv2.imwrite("in/frame.jpg", frame) # Write current frame to folder
 
-        if refCen:
-            cParts = detector.detect("in/frame.jpg", "fast")
-        detector.censor("in/frame.jpg", "out/frame.jpg", cParts)
-        frame = cv2.imread("out/frame.jpg")
+        # if refCen:
+        cParts = detector.detect("in/frame.jpg", "fast") # Read in folder and detect what needs to be censored (This is the really heavy part)
+        detector.censor("in/frame.jpg", "out/frame.jpg", cParts) # Apply censor over frame and output to folder (Also quite heavy)
+        frame = cv2.imread("out/frame.jpg") # Read back in the frame
 
-    ret, buffer = cv2.imencode('.jpg', frame)
-    bufferedFrame = buffer.tobytes()
+    ret, buffer = cv2.imencode('.jpg', frame) # Encode frame into memory buffer
+    bufferedFrame = buffer.tobytes() # Convert buffered frame to byte string
     return frame, bufferedFrame
+# </editor-fold>
 
-
+# <editor-fold desc="app routes">
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -174,13 +178,17 @@ def toggleMotion():
     return ('nothing')
 
 
-@app.route('/remakeBackground')
-def remakeBackground():
-    global refresh
-    refresh = 0
+@app.route('/autoRemakeBackground')
+def autoRemakeBackground():
+    global backgroundRefresh
+    if backgroundRefresh:
+        backgroundRefresh = False
+    else:
+        backgroundRefresh = True
     return ('nothing')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+# </editor-fold>
